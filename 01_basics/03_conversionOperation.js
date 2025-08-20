3b1f3a663c090bf310e935a0890f29fdd4bf73bd
sudhirsisodiya <sudhirsinghsisodiya123@gmail.com>
2025-08-20 16:02:40 +0000

    Convesion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score=33
+//console.log(typeof score);
+
+let score2="33"
+//console.log(typeof score2)
+let valueInNumber=Number(score2)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/* 
+33=33
+"33abc"=NaN
+true = 1
+false=0
+null=0
+undefined=NaN
+ */
+
+let isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 == boolean/true
+// 0==boolean/false
+//""==boolean/false
+//"hitesh"==boolean/true;
