a5522e90e72f7d8f2ef6b67fa7172fd97ee53f0a
sudhirsisodiya <sudhirsinghsisodiya123@gmail.com>
2025-08-28 06:57:25 +0000

    singlton objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,59 @@
+// Singletone and constructor objects
+
+// const tinderUser=new Object()            // singleton object
+    const tinderUser={}                     // non singleton object
+    tinderUser.id=451335,
+    tinderUser.name="Harry",
+    tinderUser.isLoggedin=false
+
+// console.log(tinderUser);
+
+const regularUser={
+    email:"sudhir@google.com",
+    fullname:{
+        userfullname:{
+            firstname:"sudhir",
+            middlename:"singh",
+            lastname:"sisodiya"
+        }
+    }
+}
+// console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.middlename);
+// console.log(regularUser.fullname.userfullname.lastname);
+
+const obj1={1:"a" , 2:"b"}
+const obj2={3:"c" , 4:"d"}
+
+// const obj3={obj1,obj2}
+// const obj3=Object.assign({},obj1,obj2)
+// const obj3={...obj1,...obj2}
+// console.log(obj3);
+
+const users=[
+    {
+
+        id:1,
+        email:"sudhir@g.com"
+    },
+    {
+        id:2,
+        email:"raj@g.com"
+    },{
+        id:3,
+        email:"john@g.com"
+    },{
+        id:4,
+        email:"harry@g.com"
+    }
+]
+
+users[0].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+
+console.log(tinderUser.hasOwnProperty('isLoggedin'));
