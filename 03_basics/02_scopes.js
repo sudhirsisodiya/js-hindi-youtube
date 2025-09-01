7d8afccd9517b94da1a2f8d5bfc2b7799840f3d9
sudhirsisodiya <sudhirsinghsisodiya123@gmail.com>
2025-09-01 08:11:50 +0000

    Scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,17 @@
+var c=300;
+
+{
+    if(true){
+        let a=10;
+        const b=20;
+        
+      console.log("INNER:",a);
+
+    }
+}
+
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
