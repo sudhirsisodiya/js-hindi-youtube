041c461e70afcdc5fe48531bc25c0f609ca59375
sudhirsisodiya <sudhirsinghsisodiya123@gmail.com>
2025-09-02 16:30:12 +0000

    Scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,7 +5,7 @@ var c=300;
         let a=10;
         const b=20;
         
-      console.log("INNER:",a);
+      // console.log("INNER:",a);
 
     }
 }
@@ -13,5 +13,43 @@ var c=300;
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
 
+function one(){
+  const username="sudhir"
+
+  function Two(){
+    const website="youtube"
+    console.log(username);
+    
+  }
+  // console.log(website);
+  Two()
+}
+one()
+
+if(true){
+  const username="sudhir"
+  if(username=="sudhir"){
+    const website=" linkdin "
+    console.log(username + website);
+    
+  }
+  // console.log(website);
+  
+}
+// console.log(username);
+
+// ++++++++++++++++++++++++++++++++ "Intersting" ++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(addone(5));                   
+function addone(num){                           //// It is used both(after & before) decalaration
+  return num+1
+}
+
+
+const addTwo=function(num){
+  return num+2                                // It is not used before decalaration
+}
+
+ console.log(addTwo(5));              
