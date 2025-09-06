43d182d6b9db9224c0ea115bc28dfb30d0584a6a
sudhirsisodiya <sudhirsinghsisodiya123@gmail.com>
2025-09-06 18:02:56 +0000

    control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,36 @@
+// switch statements
+// syntax
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month =3;
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("febuary");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+
+    default:
+        console.log("default case match");
+        
+        break;
+}
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,51 @@
+const userEmail=[]
+
+// if(userEmail){
+//     console.log("GOt user Email");
+    
+// }else{
+//     console.log("don't have user email");
+    
+// }
+
+// falsy values
+
+// false , 0 ,-0 ,BigInt 0n, "" ,null , undefined ,NaN
+
+// truthy values
+// "0" , 'values' , " " , [] , {} , function() ,{}
+
+// if(userEmail.length==0){
+//     console.log("Array is empty");
+    
+// }
+
+const emptyObj={}
+
+if(Object.keys(emptyObj).length === 0){
+  // console.log("object is empty");
+    
+}
+
+// Nullish Coalscing OPeraton (??) : null undefined 
+
+let val1;
+val1 = 5 ?? 10
+// console.log(val1);
+
+val1 = null ?? 10
+// console.log(val1);
+
+val1 = undefined ?? 15
+// console.log(val1);
+
+val1 = null ?? 10 ?? 15
+// console.log(val1);
+
+// terniary Operator
+
+// condition ?? true : false
+
+const iceTeaPrice =100;
+iceTeaPrice >=80 ? console.log("less than 80") : console.log("more than 80");
+
