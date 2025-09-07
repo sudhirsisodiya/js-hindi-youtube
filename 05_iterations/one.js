87fa6265909e8ccbe91eec91bb254e67690614a7
sudhirsisodiya <sudhirsinghsisodiya123@gmail.com>
2025-09-07 11:22:04 +0000

    Loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,40 @@
+// for 
+
+for (let i = 0; i < 10; i++) {
+    const element =i;
+    if(element==5){
+        // console.log("5 is the best number");
+        
+    }
+    // console.log(element);
+}
+
+for (let i = 0; i < 10; i++) {
+    // console.log(`Outer loop value : ${i}`);
+    for (let j = 0; j <= 10; j++) {
+    // console.log(`Inner loop value : ${j} and Inner loop value : ${i}`);
+            // console.log(i + '*'+ j + '=' + i*j);
+            
+    }
+    
+}
+
+let myArray=["flash", "batman", "superman"]
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
+
+for (let i = 0; i < 20; i++) {
+    const element = i;
+    if(element==5){
+        console.log("Detected 5");
+        
+        // break;
+        continue;
+    }
+    console.log(element);
+    
+}
